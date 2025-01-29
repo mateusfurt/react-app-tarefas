377aef34e1357c8d4d2a130a72c030bb45db27cb
mateusfurt <mateus2013furt@gmail.com>
2025-01-29 02:45:05 -0300

    primeiro commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+function Buttion(props) {
+  return (
+    <button {...props} className="bg-slate-400 text-white p-2 rounded-md">
+      {props.children}
+    </button>
+  );
+}
+export default Buttion;
